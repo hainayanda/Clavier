13797232e0504555bb33cfa1f9dc9c98fec44d96
Nayanda Haberty <nayanda1@outlook.com>
2021-06-20 12:03:50 +0700

    Added Example

--- Example/Clavier/ViewController.swift
+++ Example/Clavier/ViewController.swift
@@ -11,8 +11,47 @@ import Clavier
 
 class ViewController: UIViewController {
 
+    lazy var textField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "text here"
+        return textField
+    }()
+    
+    lazy var clavierButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Clavier", for: .normal)
+        button.backgroundColor = .orange
+        button.addTarget(self, action: #selector(clavierButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        clavierButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(clavierButton)
+        NSLayoutConstraint.activate([
+            clavierButton.leftAnchor.constraint(equalTo: view.leftAnchor),
+            clavierButton.rightAnchor.constraint(equalTo: view.rightAnchor),
+            clavierButton.bottomAnchor.constraint(equalTo: view.keyboardLayoutGuide.topAnchor),
+            clavierButton.heightAnchor.constraint(equalToConstant: 36)
+        ])
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(textField)
+        NSLayoutConstraint.activate([
+            textField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 24),
+            textField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -24),
+            textField.bottomAnchor.constraint(lessThanOrEqualTo: clavierButton.topAnchor, constant: -24),
+            textField.centerYAnchor.constraint(lessThanOrEqualTo: view.centerYAnchor),
+            textField.heightAnchor.constraint(equalToConstant: 36)
+        ])
+    }
+    
+    @objc func clavierButtonTapped() {
+        if textField.isFirstResponder {
+            textField.resignFirstResponder()
+        } else {
+            textField.becomeFirstResponder()
+        }
     }
 
 }
