c4ede9b82ee1e0fa29a5e09d59184d6005a5a037
Nayanda Haberty <nayanda1@outlook.com>
2021-06-21 18:10:24 +0700

    Added Example for test

--- Example/Clavier/ViewController.swift
+++ Example/Clavier/ViewController.swift
@@ -11,8 +11,22 @@ import Clavier
 
 class ViewController: UIViewController {
 
+    lazy var coloredView1: UIView = {
+        let view = UIView()
+        view.accessibilityIdentifier = "coloredView1"
+        view.backgroundColor = .blue
+        return view
+    }()
+    
+    lazy var coloredView2: UIView = {
+        let view = UIView()
+        view.backgroundColor = .cyan.withAlphaComponent(0.5)
+        return view
+    }()
+    
     lazy var textField: UITextField = {
         let textField = UITextField()
+        textField.backgroundColor = .gray.withAlphaComponent(0.75)
         textField.placeholder = "text here"
         return textField
     }()
@@ -27,12 +41,28 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        coloredView1.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(coloredView1)
+        NSLayoutConstraint.activate([
+            coloredView1.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 48),
+            coloredView1.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -48),
+            coloredView1.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            coloredView1.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5)
+        ])
+        coloredView2.translatesAutoresizingMaskIntoConstraints = false
+        coloredView1.addSubview(coloredView2)
+        NSLayoutConstraint.activate([
+            coloredView2.leftAnchor.constraint(equalTo: coloredView1.leftAnchor),
+            coloredView2.rightAnchor.constraint(equalTo: coloredView1.rightAnchor),
+            coloredView2.bottomAnchor.constraint(equalTo: coloredView1.keyboardLayoutGuide.topAnchor),
+            coloredView2.heightAnchor.constraint(equalTo: coloredView1.heightAnchor, multiplier: 0.25)
+        ])
         clavierButton.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(clavierButton)
         NSLayoutConstraint.activate([
             clavierButton.leftAnchor.constraint(equalTo: view.leftAnchor),
             clavierButton.rightAnchor.constraint(equalTo: view.rightAnchor),
-            clavierButton.bottomAnchor.constraint(equalTo: view.keyboardLayoutGuide.topAnchor),
+            clavierButton.bottomAnchor.constraint(equalTo: view.safeKeyboardLayoutGuide.topAnchor),
             clavierButton.heightAnchor.constraint(equalToConstant: 36)
         ])
         textField.translatesAutoresizingMaskIntoConstraints = false
